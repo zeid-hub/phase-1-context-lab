58377a24f5db16b6c17ff68b6e78da7563b4ffcb
zeid <zeidhassan4@gmail.com>
2024-01-14 20:26:02 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -20,4 +20,73 @@ const allWagesFor = function () {
 
     return payable
 }
-
+// createEmployeeRecord
+function createEmployeeRecord(arr) {
+    return {
+      firstName: arr[0],
+      familyName: arr[1],
+      title: arr[2],
+      payPerHour: arr[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  // createEmployeeRecords
+  function createEmployeeRecords(arrOfArr) {
+    return arrOfArr.map(createEmployeeRecord);
+  }
+  
+  // createTimeInEvent
+  function createTimeInEvent(dateTime) {
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(dateTime.slice(-4), 10),
+      date: dateTime.split(" ")[0],
+    });
+    return this;
+  }
+  
+  // createTimeOutEvent
+  function createTimeOutEvent(dateTime) {
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(dateTime.slice(-4), 10),
+      date: dateTime.split(" ")[0],
+    });
+    return this;
+  }
+  
+  // hoursWorkedOnDate
+  function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find((event) => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find((event) => event.date === date);
+  
+    if (timeInEvent && timeOutEvent) {
+      return (timeOutEvent.hour - timeInEvent.hour) / 100;
+    }
+  
+    return 0;
+  }
+  
+  // wagesEarnedOnDate
+  function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+  }
+  
+  // allWagesFor
+  // This is a placeholder for your code. You can use the provided code here.
+  
+  // findEmployeeByFirstName
+  function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find((employee) => employee.firstName === firstName);
+  }
+  
+  // calculatePayroll
+  function calculatePayroll(employees) {
+    return employees.reduce(
+      (totalPayroll, employee) => totalPayroll + allWagesFor.call(employee),
+      0
+    );
+  }
